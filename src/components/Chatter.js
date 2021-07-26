13536039cf9d53f540d8f8883182807ca3169c4e
Palakodeti <rpalakod@38f9d3704f78.ant.amazon.com>
2021-07-26 02:14:59 -0500

    fix(error): window scope for connect

--- src/components/Chatter.js
+++ src/components/Chatter.js
@@ -5,8 +5,8 @@ export const Chatter = (props) => {
 
 	useEffect(() => {
 		showChatWindow(window, document, 'amazon_connect', 'e4ee4ccc-fdd0-4e1d-a06f-168124489f8c')
-		amazon_connect('styles', { openChat: { color: 'white', backgroundColor: '#123456' }, closeChat: { color: 'white', backgroundColor: '#123456' } });
-		amazon_connect('snippetId', 'QVFJREFIZ25JVzc0R0xQTVpWSlI0RjdyVlBselRRYnQzeW5TZEVnWGFmQnNNZ0lXbmdIVm5qdDhkbmJoazFIZWc0K203VXVZQUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNRWxPNkVMWWYyZFQ0K1d2UUFnRVFnQ3NHMVpzNVNvUm1kamZXR0pvcHFmQUlrUGNnMmVMeG92YzFUWk5XMVN2dm5GVmNQWWp1aXcrMzFzTlQ6Oi9jdG5qRy9ReUxPNHkweUIvWFlTazhzd3JWTmVwc3RxYlFkek9nWjRnRHQ0VmE4QlhHeSt0bnpKR201bkI0WFIvbjJHdG9LYUxHMGR0MXJYVGFGZUcwSlptTVB3eGZvaFJobGhsZ3VjL1BraGx0RFBCR1JsY0dSK3FQTG9VYlFGV2YwNXlwclFtMWtoZGpRbyswWUVuU0J4SExTREhPVT0=');
+		window.amazon_connect('styles', { openChat: { color: 'white', backgroundColor: '#123456' }, closeChat: { color: 'white', backgroundColor: '#123456' } });
+		window.amazon_connect('snippetId', 'QVFJREFIZ25JVzc0R0xQTVpWSlI0RjdyVlBselRRYnQzeW5TZEVnWGFmQnNNZ0lXbmdIVm5qdDhkbmJoazFIZWc0K203VXVZQUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNRWxPNkVMWWYyZFQ0K1d2UUFnRVFnQ3NHMVpzNVNvUm1kamZXR0pvcHFmQUlrUGNnMmVMeG92YzFUWk5XMVN2dm5GVmNQWWp1aXcrMzFzTlQ6Oi9jdG5qRy9ReUxPNHkweUIvWFlTazhzd3JWTmVwc3RxYlFkek9nWjRnRHQ0VmE4QlhHeSt0bnpKR201bkI0WFIvbjJHdG9LYUxHMGR0MXJYVGFGZUcwSlptTVB3eGZvaFJobGhsZ3VjL1BraGx0RFBCR1JsY0dSK3FQTG9VYlFGV2YwNXlwclFtMWtoZGpRbyswWUVuU0J4SExTREhPVT0=');
 		console.log("Done: attempting to load chat")
 	});
 
